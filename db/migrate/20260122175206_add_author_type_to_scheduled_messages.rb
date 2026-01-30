fdc85f8809791bf5ead64de0f3d40fd0a35de138
gabrieljablonski <contact@gabrieljablonski.com>
2026-01-30 13:52:48 -0300

    chore: remove migration for author_type in scheduled_messages

--- db/migrate/20260122175206_add_author_type_to_scheduled_messages.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthorTypeToScheduledMessages < ActiveRecord::Migration[7.1]
-  def change
-    add_column :scheduled_messages, :author_type, :string, null: false, default: 'User'
-    change_column_default :scheduled_messages, :author_type, from: 'User', to: nil
-    remove_foreign_key :scheduled_messages, :users, column: :author_id
-  end
-end
