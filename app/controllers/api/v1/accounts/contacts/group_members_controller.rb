86178f1c14471fd65f2e682c5838bb1bedc79b79
CayoPOliveira <cayoproliveira@gmail.com>
2026-02-24 14:51:44 -0300

    chore: refactor index action in GroupMembersController for improved conversation filtering

--- app/controllers/api/v1/accounts/contacts/group_members_controller.rb
+++ app/controllers/api/v1/accounts/contacts/group_members_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::Accounts::Contacts::GroupMembersController < Api::V1::Accounts::Contacts::BaseController
   def index
+    authorize @contact, :show?
+
+    conversations = Current.account.conversations
+                           .where(contact_id: @contact.id, status: %i[open pending])
+
     @group_members = ConversationGroupMember.active
-                                            .where(conversation: @contact.conversations)
-                                            .includes(:contact, :conversation)
+                                            .where(conversation: conversations)
+                                            .includes(:contact)
   end
 end
